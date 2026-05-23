deabd4d9085e196ae8d61d67b080bb60b87aa8a9
ablyss <jb.epluribusunix.net>
2026-05-23 13:54:55 -0400

    Update

--- haiku-supermusicthingy.cpp
+++ haiku-supermusicthingy.cpp
@@ -6085,8 +6085,8 @@ BLayoutBuilder::Group<>(fConfigGroup, B_VERTICAL, 0)
     .Add(fShuffleFavsCheckbox)
     .Add(fCompactModeConfig)
     .Add(fCmpTitle)  // Still testing these two
-    .Add(fCmpSong)   
-    .Add(fChkTheme)
+    //.Add(fCmpSong)   
+    //.Add(fChkTheme)
    	.Add(fEQToggle)
    	//.Add(fEnableladspa)  // Doesn't work as good as native mpv plugins and needs to be built into ffmpeg. But leaving in code for future debugging.
    	.Add(fEnableSpectrum)
